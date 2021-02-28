71404e0392d3ba03ddf10bd104bac84df9433fbf
ce.miyashita <ce.miyashita@live.jp>
2021-02-28 21:18:14 +0900

    WebP対応に合わせてimgタグの拡張子を置き換えるタグヘルパーを実装する

---
+++ AspNetTips.MvcSite/Extensions/HostEnvironmentExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.AspNetCore.Hosting
+{
+	public static class HostEnvironmentExtension
+	{
+		/// <summary>
+		/// ファイルの仮想パスを物理パス区切りに変換して ContentRootPath と連結して返します
+		/// </summary>
+		/// <param name="env">IWebHostEnvironment を拡張します</param>
+		/// <param name="filePath">ファイルの仮想パス</param>
+		/// <returns></returns>
+		public static string MapContentRootPath(this IWebHostEnvironment env, string filePath)
+		{
+			var path = GetPhysicalPath(filePath);
+			var result = Path.Combine(env.ContentRootPath, path);
+			return result;
+		}
+
+		/// <summary>
+		/// ファイルの仮想パスを物理パス区切りに変換して WebRootPath と連結して返します
+		/// </summary>
+		/// <param name="env">IWebHostEnvironment を拡張します</param>
+		/// <param name="filePath">ファイルの仮想パス</param>
+		/// <returns></returns>
+		public static string MapWebRootPath(this IWebHostEnvironment env, string filePath)
+		{
+			var path = GetPhysicalPath(filePath);
+			var result = Path.Combine(env.WebRootPath, path);
+			return result;
+		}
+
+		/// <summary>
+		/// ファイルの仮想パスを物理パスに変換します
+		/// </summary>
+		/// <param name="virtualPath">ファイルの仮想パス</param>
+		/// <returns></returns>
+		private static string GetPhysicalPath(string virtualPath)
+		{
+			var result = Regex.Replace(virtualPath, @"^~/|^/", string.Empty).Replace("/", @"\");
+			return result;
+		}
+	}
+}
---
+++ AspNetTips.MvcSite/TagHelpers/ReplaceWebPTagHelper.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.IO;
+using System.Text.Encodings.Web;
+
+namespace AspNetTips.MvcSite.TagHelpers
+{
+	[HtmlTargetElement(TagName, Attributes = AttributesName, TagStructure = TagStructure.WithoutEndTag)]
+	public class ReplaceWebPTagHelper : UrlResolutionTagHelper
+	{
+		private const string TagName = "img";
+		private const string AttributesName = SrcAttributeName + "," + ReplaceWebpAttributeName;
+		private const string SrcAttributeName = "src";
+		private const string ReplaceWebpAttributeName = "replace-webp";
+
+		private readonly IWebHostEnvironment _env;
+
+		public ReplaceWebPTagHelper(
+			IWebHostEnvironment env,
+			IUrlHelperFactory urlHelperFactory,
+			HtmlEncoder htmlEncoder
+		) : base(urlHelperFactory, htmlEncoder)
+		{
+			_env = env;
+		}
+
+		/// <inheritdoc />
+		public override int Order => -1001;
+
+		[HtmlAttributeName(SrcAttributeName)]
+		public string Src { get; set; }
+
+		[HtmlAttributeName(ReplaceWebpAttributeName)]
+		public bool ReplaceWebp { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			if (context == null)
+				throw new ArgumentNullException(nameof(context));
+
+			if (output == null)
+				throw new ArgumentNullException(nameof(output));
+
+			output.CopyHtmlAttribute(SrcAttributeName, context);
+
+			if (ReplaceWebp)
+			{
+				Src = output.Attributes[SrcAttributeName].Value as string;
+
+				var f1 = false;
+				if (ViewContext.HttpContext.Request.Headers.TryGetValue("accept", out var sv1))
+				{
+					f1 = sv1.ToString().Contains("image/webp");
+				}
+
+				var f2 = false;
+				if (ViewContext.HttpContext.Request.Headers.TryGetValue("user-agent", out var sv2))
+				{
+					var ua = sv2.ToString();
+					f2 = ua.Contains("AppleWebKit") && ua.Contains("Version/14.");
+				}
+
+				if (f1 | f2)
+				{
+					var virtualPath = Path.ChangeExtension(Src, "webp");
+
+					var physicalPath = _env.MapWebRootPath(virtualPath);
+
+					if (File.Exists(physicalPath))
+						Src = virtualPath;
+				}
+
+				output.Attributes.SetAttribute(SrcAttributeName, Src);
+			}
+		}
+	}
+}
